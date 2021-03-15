68935c6c14eb571f689e90cb224736e3b3186c5d
nobodynoticed <eugenejukov23@gmail.com>
2021-03-15 21:02:51 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        let conf = bracketsConfig[i].join("");
+        if (str.includes(conf) == true) {
+            str = str.replace(conf, "");
+            i = -1;
+        }
+    }
+
+    if (str == "") return true;
+    else return false;
+};
